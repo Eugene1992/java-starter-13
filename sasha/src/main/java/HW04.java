9fabf27494b2d2a46660522f4cebb098ff7c0f5a
mankunianeth <deyneko55@gmail.com>
2016-09-22 16:43:55 +0300

    Test commit.

---
+++ sasha/src/main/java/HW04.java
@@ -0,0 +1,5 @@
+/**
+ * Created by evgeniy on 22/09/16.
+ */
+public class HW04 {
+}
--- sasha/src/main/java/Task.java
+++ sasha/src/main/java/Task.java
@@ -2,7 +2,7 @@
  * Created by Student on 9/21/2016.
  */
 public class Task {
-
-
-
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
 }
