c5294a7e6a7d1b41308719dde74d6fcacab16d93
Serezha Dergachov <dergachov.serezha@gmail.com>
2016-09-23 23:26:18 +0300

    Home work (Ex.2)

--- sergey/src/main/java/hw/HW_04.java
+++ sergey/src/main/java/hw/HW_04.java
@@ -3,23 +3,21 @@ package hw;
 import java.util.Scanner;
 
 /**
- * Created by Serezha on 22.09.2016.
+ * Home work 04
  */
 public class HW_04 {
+    /**
+     * Class main
+     */
     public static void main(String[] args) {
 //ex.2
         Scanner scan = new Scanner(System.in);
-        Double inputNumber = scan.nextDouble();
-        boolean wholeNumber = inputNumber % 1 == 0 ? true : false;
+        int inputNumber = scan.nextInt();
 
-        if (wholeNumber) {
-            if (inputNumber % 2 == 0) {
-                System.out.println("Number IS WHOLE : " + inputNumber + " and EVEN.");
-            } else {
-                System.out.println("Number IS WHOLE : " + inputNumber + " and NOT even.");
-            }
+        if (inputNumber % 2 == 0) {
+            System.out.println("Number : " + inputNumber + " is EVEN.");
         } else {
-            System.out.println("Number is NOT whole : " + inputNumber);
+            System.out.println("Number : " + inputNumber + " is NOT even.");
         }
     }
 }
