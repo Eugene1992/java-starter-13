a1b747dfb78e26c14ad78f10edb293363433308c
Serezha Dergachov <dergachov.serezha@gmail.com>
2016-09-24 00:43:41 +0300

    Home work (Ex.2)

--- sergey/src/main/java/hw/HW_04.java
+++ sergey/src/main/java/hw/HW_04.java
@@ -7,17 +7,32 @@ import java.util.Scanner;
  */
 public class HW_04 {
     /**
-     * Class main
+     * main() function
      */
     public static void main(String[] args) {
 //ex.2
-        Scanner scan = new Scanner(System.in);
+       /* Scanner scan = new Scanner(System.in);
         int inputNumber = scan.nextInt();
 
         if (inputNumber % 2 == 0) {
             System.out.println("Number : " + inputNumber + " is EVEN.");
         } else {
             System.out.println("Number : " + inputNumber + " is NOT even.");
-        }
+        }*/
+
+//ex.3
+       /* Scanner scan = new Scanner(System.in);
+        Double inputNumber1 = scan.nextDouble();
+        Double inputNumber2 = scan.nextDouble();
+
+        if (inputNumber1 >= 10 & inputNumber2 > inputNumber1) {
+            System.out.println("The Number : " + inputNumber1 + " closer to 10.");
+        } else {
+            if (inputNumber1.equals(inputNumber2)) {
+                System.out.println("Number1 == Number2");
+            } else {
+                System.out.println("The Number : " + inputNumber2 + " closer to 10.");
+            }
+        }*/
     }
 }
