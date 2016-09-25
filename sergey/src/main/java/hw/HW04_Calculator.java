a147c950a44870b6f1c2f43c570623ada5a35b6c
Serezha Dergachov <dergachov.serezha@gmail.com>
2016-09-25 19:57:13 +0300

    Home work (Ex.5)

---
+++ sergey/src/main/java/hw/HW04_Calculator.java
@@ -0,0 +1,13 @@
+package hw;
+
+/**
+ * Class HW04_Calculator
+ */
+public class HW04_Calculator {
+    /**
+     * main() function
+     */
+    public static void main(String[] args) {
+
+    }
+}
--- sergey/src/main/java/hw/HW_04.java
+++ sergey/src/main/java/hw/HW_04.java
@@ -11,34 +11,36 @@ public class HW_04 {
      */
     public static void main(String[] args) {
 //ex.2
-       /* Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         int inputNumber = scan.nextInt();
 
         if (inputNumber % 2 == 0) {
-            System.out.println("Number : " + inputNumber + " is EVEN.");
+            System.out.println("Number : " + inputNumber + " is EVEN.\n");
         } else {
-            System.out.println("Number : " + inputNumber + " is NOT even.");
-        }*/
+            System.out.println("Number : " + inputNumber + " is NOT even.\n");
+        }
 
 //ex.3
-       /* Scanner scan = new Scanner(System.in);
-        Double inputNumber1 = scan.nextDouble();
-        Double inputNumber2 = scan.nextDouble();
+       Scanner scan2 = new Scanner(System.in);
+        Double inputNumber1 = scan2.nextDouble();
+        Double inputNumber2 = scan2.nextDouble();
 
         if (inputNumber1 >= 10 & inputNumber2 > inputNumber1) {
-            System.out.println("The Number : " + inputNumber1 + " closer to 10.");
+            System.out.println("The Number : " + inputNumber1 + " closer to 10.\n");
         } else {
             if (inputNumber1.equals(inputNumber2)) {
-                System.out.println("Number1 == Number2");
+                System.out.println("Number1 == Number2\n");
             } else {
-                System.out.println("The Number : " + inputNumber2 + " closer to 10.");
+                System.out.println("The Number : " + inputNumber2 + " closer to 10.\n");
             }
-        }*/
+        }
 //ex.4
-        /*
+
         int varA = -40;
         int varB = 30;
         int varC = 200;
+
+        System.out.println("varA = " + varA + "\nvarB = " + varB + "\nvarC = " + varC);
 
         if (varA > varB) {
             System.out.println("varA " + varA + " biger.");
@@ -46,18 +48,23 @@ public class HW_04 {
             if (varB > varC) {
                 System.out.println("varB " + varB + " biger.");
             } else {
-                System.out.println("varC " + varC + " biger.");
+                System.out.println("varC " + varC + " biger.\n");
             }
-        }*/
+        }
 //ex.5
         /*Задано четыре переменных, найти и вывести на экран вторую по величине.
         *                        Tests                                         */
+
         int var1 = 178;       //  1   1   1   4   3
         int var2 = 114;       //  2   2   4   3   4
         int var3 = 147;       //  3   4   3   2   2
         int var4 = 247;       //  4   3   2   1   1
 
         System.out.println("var1 = " + var1 + "\nvar2 = " + var2 + "\nvar3 = " + var3 + "\nvar4 = " + var4);
+        /*
+         * Решение: Сравнить каждую переменную с остальными при помощи логических операторов и операторов сравнения в конструкции else-if
+         */
+
         // при условии что 1 < 2, но 2 больше остальных и 1 > 3,4                ИЛИ          при условии что 1 < 3, но 3 больше остальных и 1 > 2 и 4    ИЛИ  при условии что 1 < 4, но 4 больше остальных и 1 > 2 и 3
         if ((var1 < var2 & var2 > var3 & var2 > var4 & var1 > var3 & var1 > var4) | (var1 < var3 & var3 > var2 & var3 > var4 & var1 > var2 & var1 > var4) || (var1 < var4 & var4 > var3 & var4 > var2 & var1 > var2 & var1 > var3)) {
             System.out.println("----------> var1 = " + var1);
@@ -76,5 +83,8 @@ public class HW_04 {
         } else {
             System.out.println("false");
         }
+
+//ex.6
+
     }
 }
