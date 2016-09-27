39343937ef672ec72e8fbbee97a39a841120b87e
Aleksandr Maretskyi <aleeksandr.maretskij@yandex.ru>
2016-09-27 10:19:20 +0300

    initial commit

---
+++ sasha/src/main/java/cw_5/CW_5.java
@@ -0,0 +1,14 @@
+/**package cw_5;
+
+/**
+ * Created by adnin on 26.09.2016.
+
+public class CW_5 {
+    public static void main(String [] args) {
+        int x =
+        for (int )
+        System.out.println();
+
+    }
+
+}*/
