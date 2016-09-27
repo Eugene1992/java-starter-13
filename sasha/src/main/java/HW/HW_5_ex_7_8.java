c15fb96e89cc8c092238dbcec98c9e44cb7a2653
Aleksandr Maretskyi <aleeksandr.maretskij@yandex.ru>
2016-09-27 13:00:04 +0300

    initial commit

---
+++ sasha/src/main/java/HW/HW_5_ex_7_8.java
@@ -0,0 +1,19 @@
+package HW;
+
+import java.util.Scanner;
+
+/**
+ * Created by adnin on 27.09.2016.
+ */
+public class HW_5_ex_7_8 {
+    public static void main(String[] args) {
+
+        /**7. Создайте программу, вычисляющую факториал натурального числа n,
+         введенного пользователем. */
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+       // for (n = 1; n > 0; n =  )
+    }
+}
