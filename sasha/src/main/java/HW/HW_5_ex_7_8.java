bdab38733dd1deb92e8688b345dbe2076a762e00
Aleksandr Maretskyi <aleeksandr.maretskij@yandex.ru>
2016-09-27 13:12:36 +0300

    initial commit

--- sasha/src/main/java/HW/HW_5_ex_7_8.java
+++ sasha/src/main/java/HW/HW_5_ex_7_8.java
@@ -12,8 +12,10 @@ public class HW_5_ex_7_8 {
          введенного пользователем. */
 
         Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
+        long n = scan.nextLong();
+        int i = scan.nextInt();
 
-       // for (n = 1; n > 0; n =  )
+        for (n = 1; n > 0; n = n * i)
+            System.out.println("n = " + n);
     }
 }
